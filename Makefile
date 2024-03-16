028f0314026d04831b0b482fd90de8840a3d9bc8
Mtbanban <128633147+MtBanBann@users.noreply.github.com>
2024-03-16 21:27:22 +0100

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,94 @@
+
+NAME := so_long
+SRC_DIR := src
+BUILD_DIR := .build
+DEPS_DIR := .deps
+
+INC_DIR = inc
+INC := $(INC_DIR)/so_long.h \
+	   $(INC_DIR)/so_long_main.h \
+	   $(INC_DIR)/so_long_init.h \
+	   $(INC_DIR)/so_long_keys.h
+
+MLX_DIR = minilibx-linux
+MLX_INC = $(MLX_DIR)/mlx.h
+
+IFLAGS = -I $(INC_DIR) \
+		 -I $(MLX_DIR)
+
+SO_LONG =	$(SRC_DIR)/so_long.c \
+			$(SRC_DIR)/map_test.c \
+			$(SRC_DIR)/map_good.c \
+			$(SRC_DIR)/map_edge.c \
+			$(SRC_DIR)/map_init.c \
+			$(SRC_DIR)/init_mlx_var.c \
+			$(SRC_DIR)/mlx_error.c \
+			$(SRC_DIR)/mlx_keys.c \
+			$(SRC_DIR)/map_size.c \
+			$(SRC_DIR)/mlx_free.c
+
+SRC := $(SO_LONG)
+
+OBJS    := $(patsubst $(SRC_DIR)/%.c,$(BUILD_DIR)/%.o,$(SRC))
+DEPS    := $(patsubst %.o, %.d,$(OBJS))
+
+LIBS       				:= libft 
+
+LIBS_TARGET 			:= libft/libft.a 
+
+DIR_MLX		:=	minilibx_linux
+
+MLX_TARGET :=			minilibx-linux/libmlx_Linux.a 
+
+CC := cc -g3
+CFLAGS := -Wall -Wextra -Werror
+CPPFLAGS := -MD -MP
+LDFLAGS := $(addprefix -L,$(dir $(LIBS_TARGET))) \
+			 $(addprefix -L,$(dir $(MLX_TARGET)))
+
+LDLIBS := -lmlx_Linux -lft -lX11 -lXext  -lm 
+
+RM := rm -f
+
+all: $(NAME) 
+
+FORCE:
+
+
+$(LIBS_TARGET): FORCE
+	$(MAKE) -C $(LIBS) all
+
+$(MLX_TARGET): FORCE
+	$(MAKE) -C $(MLX_DIR) all
+
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.c $(LIBS_TARGET) $(MLX_TARGET)
+		mkdir -p $(@D)
+		$(CC) $(CFLAGS) $(CPPFLAGS) $(IFLAGS) -c $< -o $@ 
+
+$(NAME): $(OBJS) $(LIBS_TARGET) $(MLX_TARGET)
+			$(CC) $(LDFLAGS) $(OBJS)  $(LDLIBS) -o $(NAME) 
+
+
+-include $(DEPS)
+
+# **************************************************************************** #
+
+clean:
+	$(MAKE) -C $(LIBS) fclean
+	$(MAKE) -C $(MLX_DIR) clean
+	$(RM) -r $(BUILD_DIR) $(DEPS_DIR)
+
+# **************************************************************************** #
+
+fclean: clean
+	$(RM) -r $(NAME)
+
+# **************************************************************************** #
+
+re: fclean all
+
+# **************************************************************************** # # .PHONY #
+
+.PHONY: all clean fclean re
+
+# *****************************
---
+++ src/init_mlx_var.c
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_mlx_var.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afavier <afavier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 11:10:56 by afavier           #+#    #+#             */
+/*   Updated: 2024/03/12 22:46:29 by afavier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static void	vars_put_img(t_vars *new_vars, t_map *map, int img_width,
+				int img_height);
+static void	vars_put_img(t_vars *new_vars, t_map *map, int img_width,
+				int img_height);
+
+t_vars	*initialize_vars(t_map *map)
+{
+	t_vars	*new_vars;
+
+	new_vars = malloc(sizeof(t_vars));
+	if (!new_vars)
+		error_free_map(map);
+	new_vars->mlx = mlx_init();
+	if (new_vars->mlx == NULL)
+		error_mlx(map, new_vars);
+	new_vars->win = mlx_new_window(new_vars->mlx, 1900, 500, "so long");
+	if (new_vars->win == NULL)
+		error_mlx(map, new_vars);
+	new_vars->map = map;
+	new_vars->fond.img = NULL;
+	new_vars->escape.img = NULL;
+	new_vars->perso.img_perso.img = NULL;
+	vars_put_img(new_vars, map, 100, 100);
+	new_vars->perso.width_perso = 0;
+	new_vars->perso.height_perso = 0;
+	new_vars->perso.x = 0;
+	new_vars->perso.y = 0;
+	new_vars->perso.height = 0;
+	new_vars->perso.width = 0;
+	new_vars->nb_mouv = 0;
+	return (new_vars);
+}
+
+t_data	*initialize_wall(t_vars *vars, t_map *map)
+{
+	char	*wall;
+	t_data	*new_wall;
+
+	wall = "./pics/wall.xpm";
+	new_wall = malloc(sizeof(t_data));
+	if (!new_wall)
+		error_mlx_wall(map, vars, new_wall, 1);
+	new_wall->img_width = 0;
+	new_wall->img_height = 0;
+	new_wall->img = mlx_xpm_file_to_image(vars->mlx, wall, &new_wall->img_width,
+			&new_wall->img_height);
+	if (!new_wall->img)
+		error_mlx_wall(map, vars, new_wall, 1);
+	new_wall->addr = mlx_get_data_addr(new_wall->img, &new_wall->bits_per_pixel,
+			&new_wall->line_length, &new_wall->endian);
+	if (new_wall->addr == NULL)
+		error_mlx_wall(map, vars, new_wall, 1);
+	return (new_wall);
+}
+
+t_data	*initialize_collect(t_vars *vars, t_map *map, t_data *wall)
+{
+	char	*collectible;
+	t_data	*new_collec;
+
+	collectible = "./pics/collectible.xpm";
+	new_collec = malloc(sizeof(t_data));
+	if (!new_collec)
+		error_mlx_collect(map, vars, wall, new_collec);
+	new_collec->img_width = 0;
+	new_collec->img_height = 0;
+	new_collec->img = mlx_xpm_file_to_image(vars->mlx, collectible,
+			&new_collec->img_width, &new_collec->img_height);
+	if (!new_collec->img)
+		error_mlx_collect(map, vars, wall, new_collec);
+	new_collec->addr = mlx_get_data_addr(new_collec->img,
+			&new_collec->bits_per_pixel,
+			&new_collec->line_length,
+			&new_collec->endian);
+	if (new_collec->addr == NULL)
+		error_mlx_collect(map, vars, wall, new_collec);
+	return (new_collec);
+}
+
+static void	vars_put_img_following(t_vars *new_vars, t_map *map)
+{
+	new_vars->fond.addr = mlx_get_data_addr(new_vars->fond.img,
+			&new_vars->fond.bits_per_pixel,
+			&new_vars->fond.line_length,
+			&new_vars->fond.endian);
+	if (new_vars->fond.addr == NULL)
+		error_mlx(map, new_vars);
+	new_vars->escape.addr = mlx_get_data_addr(new_vars->escape.img,
+			&new_vars->escape.bits_per_pixel,
+			&new_vars->escape.line_length,
+			&new_vars->escape.endian);
+	if (new_vars->escape.addr == NULL)
+		error_mlx(map, new_vars);
+	new_vars->perso.img_perso.addr = mlx_get_data_addr(
+			new_vars->perso.img_perso.img,
+			&new_vars->perso.img_perso.bits_per_pixel,
+			&new_vars->perso.img_perso.line_length,
+			&new_vars->perso.img_perso.endian);
+	if (new_vars->perso.img_perso.addr == NULL)
+		error_mlx(map, new_vars);
+}
+
+static void	vars_put_img(t_vars *new_vars, t_map *map, int img_width,
+		int img_height)
+{
+	char	*perso;
+	char	*background;
+	char	*escape;
+
+	perso = "./pics/perso.xpm";
+	background = "./pics/fond.xpm";
+	escape = "./pics/escape.xpm";
+	new_vars->fond.img = mlx_xpm_file_to_image(new_vars->mlx, background,
+			&img_width, &img_height);
+	if (new_vars->fond.img == NULL)
+		error_mlx(map, new_vars);
+	new_vars->escape.img = mlx_xpm_file_to_image(new_vars->mlx, escape,
+			&img_width, &img_height);
+	if (new_vars->escape.img == NULL)
+		error_mlx(map, new_vars);
+	new_vars->perso.img_perso.img = mlx_xpm_file_to_image(new_vars->mlx, perso,
+			&img_width, &img_height);
+	if (new_vars->perso.img_perso.img == NULL)
+		error_mlx(map, new_vars);
+	vars_put_img_following(new_vars, map);
+}
